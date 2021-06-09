31d7dfeead464b32f90efe525e887c2e9489896c
Mohmad <magomedov951@mail.ru>
2021-06-09 15:45:11 +0300

    model Channel

---
+++ models/Channel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose")
+
+const Channel = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  name:  {
+    type: String,
+    required: true
+  },
+  text:  {
+    type: String,
+    required: true
+  },
+  category: {id: mongoose.Types.ObjectId, ref: "Category"},
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model("Channel", Channel)
